ec3f559b689d7c31ba453574a9648a446cd99b36
pawanidhiman712 <https://github.com/pawanidhiman712/Java-code.git>
2026-05-17 16:41:34 +0530

    first commit

---
+++ SortArray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class SortArray {
+    public static void main(String[] args) {
+        int a[]={5,2,1,4};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
