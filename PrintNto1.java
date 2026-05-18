d5bd560706513ad554fbb9daa0ceaa723799cbc7
pawanidhiman712 <https://github.com/pawanidhiman712/Java-code.git>
2026-05-18 11:41:44 +0530

    Updated Java codes

---
+++ PrintNto1.java
@@ -0,0 +1,9 @@
+class PrintNto1 {
+    public static void main(String[] args) {
+        int n = 10;
+
+        for(int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
